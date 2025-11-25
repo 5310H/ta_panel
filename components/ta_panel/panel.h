2ec5fb3c4d44e7d3a956f5e21087b5ee89e50e7e
5310H <5310@trahreg.com>
2025-11-25 18:39:46 -0500

    Add TaPanel class to panel.h

---
+++ components/ta_panel/panel.h
@@ -0,0 +1,14 @@
+#pragma once
+#include "esphome/core/component.h"
+
+namespace esphome {
+namespace ta_panel {
+
+class TaPanel : public Component {
+ public:
+  void setup() override {}
+  void loop() override {}
+};
+
+}  // namespace ta_panel
+}  // namespace esphome
